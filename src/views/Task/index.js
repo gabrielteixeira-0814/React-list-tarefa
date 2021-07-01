f29d1d044b809f1808ae4b992bfd55f643c97e5b
gabrielteixeira-0814 <gabrielteixeira0814@gmail.com>
2021-07-01 11:59:12 -0300

    Criando Formulário

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -8,12 +8,15 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import TypeIcons from '../../utils/typeIcons';
 
+import iconCalendar from '../../assets/calendar.png';
+import iconClock from '../../assets/clock.png';
+
 function Task() {
   const [lateCount, setLateCount] = useState();
   const [type, setType] = useState();
 
   async function lateVerify() {
-    await api.get(`/task/filter/late/11-11-11-11-11-11`)
+    await api.get(`/task/filter/late/11:11:11:11:11:11`)
     .then(response => {
       setLateCount(response.data.length);
       console.log(response.data.length);
@@ -42,11 +45,33 @@ function Task() {
 
         <S.Input>
           <span>Título</span>
-          <input type="text" placeholder="Título da tarefa..."></input>
-          
+          <input type="text" placeholder="Título da tarefa..." />
         </S.Input>
+        <S.TextArea>
+          <span>Descrição</span>
+          <textarea rows={5} placeholder="Título da tarefa..." />
+        </S.TextArea>
+        <S.Input>
+          <span>Data</span>
+          <input type="date" placeholder="Título da tarefa..." />
+          {/* <img src={iconCalendar} alt="Calendário" /> */}
+        </S.Input>
+        <S.Input>
+          <span>Hora</span>
+          <input type="time" placeholder="Título da tarefa..." />
+          {/* <img src={iconClock} alt="Horas" /> */}
+        </S.Input>
+        <S.Options>
+          <div>
+            <input type="checkbox" />
+            <span>CONCLUÍDO</span>
+          </div>
+          <button type="button">EXCLUIR</button>
+        </S.Options>
+        <S.Save>
+          <button type="button">Salvar</button>
+        </S.Save>
       </S.Form>
-
       <Footer />
     </S.Container>
 
--- src/views/Task/styles.js
+++ src/views/Task/styles.js
@@ -9,6 +9,7 @@ export const Container = styled.div`
 
 export const Form = styled.div`
     width: 50%;
+    margin-bottom: 70px;
 `
 
 export const TypeIcons = styled.div`
@@ -35,7 +36,7 @@ export const TypeIcons = styled.div`
         &:hover {
             opacity: 0.5;
         }
-    }
+}
 
 `
 
@@ -43,17 +44,92 @@ export const Input = styled.div`
    width: 100%;
    display: flex;
    flex-direction: column;
+   margin: 20px 0;
 
    span {
     color: #707070;
-    margin-bottom: 5px;
+    margin-bottom: 5px 0;
 
-   }
+}
 
    input {
        font-size: 16px;
        padding: 15px;
        border: none;
-       border-bottom: #
+       border-bottom: 1px solid #EE6B26;
+}
+
+   img {
+       width: 20px;
+       height: 20px;
+       position: relative;
+       left: 94%;
+       bottom: 36px;
+
+}
+`
+export const TextArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+
+    span {
+    color: #707070;
+    margin-bottom: 5px 0;
    }
+
+   textarea {
+    font-size: 16px;
+    border: 1px solid #EE6B26;
+    padding: 15px;
+}
+`
+
+export const Options = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    div {
+    display: flex;
+    align-items: center;
+    color: #EE6B26;
+    font-weight: bold;
+    font-size: 18px;
+}
+
+   button {
+    font-weight: bold;
+    color: #20295F;
+    border: none;
+    background: none;
+    font-size: 18px;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.7;
+    }
+}
+`
+
+export const Save = styled.div`
+    width: 100%;
+    margin-top: 20px;
+
+    button {
+        width: 100%;
+        background-color: #EE6B26;
+        border: none;
+        font-size: 20px;
+        color: #FFF;
+        font-weight: bold;
+        padding: 20px;
+        border-radius: 30px;
+        cursor: pointer;
+
+        &:hover {
+        opacity: 0.7;
+    }
+}
 `
